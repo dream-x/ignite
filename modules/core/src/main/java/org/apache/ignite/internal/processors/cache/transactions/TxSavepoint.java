065dca133371ea216737b1cf83f9d93fd18bd71f
Dmitrii Ryabov <SomeFireOne@gmail.com>
2017-04-13 18:42:57 +0300

    TxSavepoint javadoc.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/transactions/TxSavepoint.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/transactions/TxSavepoint.java
@@ -18,7 +18,40 @@
 package org.apache.ignite.internal.processors.cache.transactions;
 
 /**
+ * A savepoint is a special mark inside a transaction that allows all commands
+ * that are executed after it was established to be rolled back,
+ * restoring the transaction state to what it was at the time of the savepoint.
  *
+ * <h1 class="header">Usage</h1>
+ *
+ * The execution result guarantee that only values 1 and 3 are inserted into cache:
+ *
+ * <pre name="code" class="java">
+ * Ignite ignite = ....;
+ * IgniteCache<Integer, Integer> c = ....;
+ *
+ * try (Transaction tx = ignite.transactions().txStart()) {
+ *     c.put(1, 1);
+ *
+ *     tx.savepoint("mysavepoint");
+ *
+ *     c.put(2, 2);
+ *
+ *     tx.rollbackToSavepoint("mysavepoint");
+ *
+ *     c.put(3, 3);
+ *
+ *     tx.commit();
+ * }
+ * </pre>
+ *
+ * <h1 class="header">Restrictions</h1>
+ *
+ * <ul>
+ * <li>Savepoints works only with {@code TRANSACTIONAL} caches on the node where transaction started.</li>
+ * <li>In case of {@code ATOMIC} caches and caches from other nodes - they will be ignored.
+ * There is no need in savepoints because every action commits immediately.</li>
+ * </ul>
  */
 public interface TxSavepoint {
 
